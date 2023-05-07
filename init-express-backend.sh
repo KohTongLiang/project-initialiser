01f343bcaa8f75163f7f2bcebe843fbb39cdcd2e
KohTongLiang <koh.tongliang@gmail.com>
2023-05-07 22:03:18 +0800

    Added lodash, helmet, rate-limiter, logger, body-parser.

--- init-express-backend.sh
+++ init-express-backend.sh
@@ -14,7 +14,7 @@ cd "$project_name"
 npm init -y
 
 # Install dependencies
-npm i express cors dotenv
+npm i express cors dotenv helmet express-rate-limit winston winston-daily-rotate-file lodash body-parser
 npm i --save-dev typescript ts-node nodemon @types/node @types/express @types/cors eslint prettier eslint-plugin-import eslint-plugin-prettier eslint-plugin-simple-import-sort @typescript-eslint/eslint-plugin @typescript-eslint/parser
 
 # Create environment variables file
@@ -90,13 +90,18 @@ EOL
 npx json -I -f package.json -e 'this.scripts={ "dev": "nodemon --watch src --exec ts-node src/index.ts", "build": "tsc", "serve": "node dist/index.js", "lint": "eslint --ext .ts src/", "format": "prettier --write \"src/**/*.ts\"", "lint:fix": "eslint --ext .ts src/ --fix"}'
 
 # Create directories and files
-mkdir -p src/controllers src/models src/services src/routes
+mkdir -p logs/ src/controllers src/models src/services src/routes
 
 # Create index.ts with a sample code
 cat > src/index.ts <<EOL
 import dotenv from "dotenv";
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import DailyRotateFile from "winston-daily-rotate-file";
+import winston from "winston";
 import sampleRoutes from './routes/sampleRoutes';
 
 dotenv.config(); // Load environment variables
@@ -106,6 +111,69 @@ const port : string = process.env.PORT || "3000";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// setup helmet
+app.use(helmet());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.hsts({
+  maxAge: 31536000,
+	  includeSubDomains: true,
+		  preload: true
+		}));
+		app.use(helmet.contentSecurityPolicy({
+		  directives: {
+			    defaultSrc: ["'self'"],
+					    styleSrc: ["'self'", 'https://fonts.googleapis.com'],
+							    fontSrc: ["'self'", 'https://fonts.gstatic.com']
+									  }
+								}));
+
+// setup rate-limiting
+const limiter = rateLimit({
+	 windowMs: 1 * 60 * 1000, // 1 minute
+	 max: 100, // 100 requests per minute
+});
+app.use(limiter);
+
+// setup logger
+const logger = winston.createLogger({
+	level: "info",
+	format: winston.format.combine(
+		winston.format.timestamp({
+			format: "YYYY-MM-DD HH:mm:ss",
+		}),
+		winston.format.errors({ stack: true }),
+		winston.format.splat(),
+		winston.format.json()
+	),
+	transports: [
+		new DailyRotateFile({
+			filename: "logs/application-%DATE%.log",
+			datePattern: "YYYY-MM-DD",
+			zippedArchive: true,
+			maxSize: "20m",
+			maxFiles: "14d",
+		}),
+	],
+});
+
+// log requests
+app.use((req, res, next) => {
+	logger.info({
+		message: "Request URL",
+		url: req.url,
+		method: req.method,
+		query: req.query,
+		headers: req.headers,
+		body: req.body,
+	});
+	next();
+});
 
 app.listen(port, () => {
    console.log(\`Server is running on port \${port}\`);
